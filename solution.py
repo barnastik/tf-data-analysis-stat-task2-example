02c4864ea595e69803797a8cb9a297e0a44f303e
barnastik <113926737+barnastik@users.noreply.github.com>
2023-04-07 17:46:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,20 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1004689536 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+
+    from scipy.stats import t
+    data = x
+    alpha = p
+    n = len(data)
+    mean = np.mean(data)
+    std_err = np.std(data, ddof=1) / np.sqrt(n)
+    t_score = t.ppf(1 - alpha / 2, n - 1)
+    lower_bound = mean - t_score * std_err
+    upper_bound = mean + t_score * std_err
+    return (lower_bound, upper_bound)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
